947ddd7f966f4a6f399c8d371782fad1339e5ffa
Hacker78909 <98651693+Hacker78909@users.noreply.github.com>
2023-07-09 12:00:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,126 +1,135 @@
-window.onload = function () {
-  // BouncingBallGame class
-  function BouncingBallGame() {
-    var canvas = document.getElementById("gameCanvas");
-    var context = canvas.getContext("2d");
+window.onload = function() {
+  // Get the canvas element and create a 2D drawing context
+  var canvas = document.getElementById("gameCanvas");
+  var context = canvas.getContext("2d");
 
-    var paddleX = 200;
-    var paddleY = 450;
-    var ballX;
-    var ballY;
-    var ballXSpeed = 1;
-    var ballYSpeed = 1;
-    var score = 0;
-    var highestScore = 0;
-    var gameOver = false;
-    var restartButton;
-    var timer;
-    var random;
+  // Game variables
+  var paddleX = 200;
+  var paddleY = 450;
+  var ballX;
+  var ballY;
+  var ballXSpeed = 1;
+  var ballYSpeed = 1;
+  var score = 0;
+  var highestScore = 0;
+  var gameOver = false;
+  var restartButton;
+  var timer;
+  var random;
 
-    function init() {
-      canvas.addEventListener("keydown", keyPressed);
-      canvas.addEventListener("keyup", keyReleased);
-      restartButton = document.createElement("button");
-      restartButton.innerHTML = "Restart";
-      restartButton.style.position = "absolute";
-      restartButton.style.left = "200px";
-      restartButton.style.top = "225px";
-      restartButton.style.width = "100px";
-      restartButton.style.height = "50px";
-      restartButton.style.visibility = "hidden";
-      restartButton.addEventListener("click", restartGame);
-      document.body.appendChild(restartButton);
+  // Initialize the game
+  function initGame() {
+    // Set up the canvas and game objects
+    random = new Random();
+    ballX = random.nextInt(canvas.width - 20);
+    ballY = random.nextInt(canvas.height - 20);
 
-      random = new Random();
-      ballX = random.nextInt(canvas.width - 20);
-      ballY = random.nextInt(canvas.height - 20);
+    // Create a restart button and hide it initially
+    restartButton = document.createElement("button");
+    restartButton.innerHTML = "Restart";
+    restartButton.style.position = "absolute";
+    restartButton.style.left = "200px";
+    restartButton.style.top = "225px";
+    restartButton.style.width = "100px";
+    restartButton.style.height = "50px";
+    restartButton.style.display = "none";
+    restartButton.addEventListener("click", restartGame);
+    document.body.appendChild(restartButton);
 
-      timer = setInterval(update, 10);
-    }
-
-    function update() {
-      moveBall();
-
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
-      context.fillStyle = "blue";
-      context.beginPath();
-      context.arc(ballX, ballY, 10, 0, Math.PI * 2);
-      context.closePath();
-      context.fill();
-
-      context.fillStyle = "red";
-      context.fillRect(paddleX, paddleY, 80, 10);
-
-      context.fillStyle = "black";
-      context.font = "16px Arial";
-      context.fillText("Score: " + score, 10, 20);
-      context.fillText("Highest Score: " + highestScore, 10, 40);
-
-      if (gameOver) {
-        context.fillStyle = "black";
-        context.font = "20px Arial";
-        context.fillText("Game Over", 200, 200);
-        clearInterval(timer);
-        restartButton.style.visibility = "visible";
-      }
-    }
-
-    function moveBall() {
-      ballX += ballXSpeed;
-      ballY += ballYSpeed;
-
-      if (ballX <= 0 || ballX >= canvas.width - 20) {
-        ballXSpeed = -ballXSpeed;
-      }
-      if (ballY <= 0 || ballY >= canvas.height - 20) {
-        ballYSpeed = -ballYSpeed;
-      }
-
-      if (
-        ballX < paddleX + 80 &&
-        ballX + 20 > paddleX &&
-        ballY < paddleY + 10 &&
-        ballY + 20 > paddleY
-      ) {
-        ballYSpeed = -ballYSpeed;
-        score++;
-      } else if (ballY >= canvas.height - 20) {
-        gameOver = true;
-      }
-    }
-
-    function restartGame() {
-      gameOver = false;
-      restartButton.style.visibility = "hidden";
-      ballX = random.nextInt(canvas.width - 20);
-      ballY = random.nextInt(canvas.height - 20);
-      ballXSpeed = 1;
-      ballYSpeed = 1;
-      score = 0;
-      timer = setInterval(update, 10);
-    }
-
-    function keyPressed(event) {
-      if (event.keyCode === 37) {
-        // Left arrow key
-        paddleX -= 10;
-      } else if (event.keyCode === 39) {
-        // Right arrow key
-        paddleX += 10;
-      }
-
-      if (paddleX < 0) {
-        paddleX = 0;
-      } else if (paddleX > canvas.width - 80) {
-        paddleX = canvas.width - 80;
-      }
-    }
-
-    function keyReleased(event) {}
-
-    init();
+    // Start the game loop
+    timer = setInterval(updateGame, 10);
   }
 
-  new BouncingBallGame();
+  // Update the game state
+  function updateGame() {
+    moveBall();
+
+    // Clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the ball
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.arc(ballX, ballY, 10, 0, 2 * Math.PI);
+    context.fill();
+
+    // Draw the paddle
+    context.fillStyle = "red";
+    context.fillRect(paddleX, paddleY, 80, 10);
+
+    // Draw the score
+    context.fillStyle = "black";
+    context.font = "16px Arial";
+    context.fillText("Score: " + score, 10, 20);
+    context.fillText("Highest Score: " + highestScore, 10, 40);
+
+    // Check if the game is over
+    if (gameOver) {
+      // Stop the game loop
+      clearInterval(timer);
+
+      // Display game over message and show the restart button
+      context.fillStyle = "black";
+      context.font = "20px Arial";
+      context.fillText("Game Over", 200, 200);
+      restartButton.style.display = "block";
+    }
+  }
+
+  // Move the ball and handle collisions
+  function moveBall() {
+    ballX += ballXSpeed;
+    ballY += ballYSpeed;
+
+    if (ballX <= 0 || ballX >= canvas.width - 20) {
+      ballXSpeed = -ballXSpeed;
+    }
+    if (ballY <= 0 || ballY >= canvas.height - 20) {
+      ballYSpeed = -ballYSpeed;
+    }
+
+    if (
+      ballX < paddleX + 80 &&
+      ballX + 20 > paddleX &&
+      ballY < paddleY + 10 &&
+      ballY + 20 > paddleY
+    ) {
+      ballYSpeed = -ballYSpeed;
+      score++;
+    } else if (ballY >= canvas.height - 20) {
+      gameOver = true;
+    }
+  }
+
+  // Restart the game
+  function restartGame() {
+    gameOver = false;
+    restartButton.style.display = "none";
+    ballX = random.nextInt(canvas.width - 20);
+    ballY = random.nextInt(canvas.height - 20);
+    ballXSpeed = 1;
+    ballYSpeed = 1;
+    score = 0;
+    timer = setInterval(updateGame, 10);
+  }
+
+  // Handle key press events
+  document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 37) {
+      // Left arrow key
+      paddleX -= 10;
+    } else if (event.keyCode === 39) {
+      // Right arrow key
+      paddleX += 10;
+    }
+
+    if (paddleX < 0) {
+      paddleX = 0;
+    } else if (paddleX > canvas.width - 80) {
+      paddleX = canvas.width - 80;
+    }
+  });
+
+  // Start the game
+  initGame();
 };
